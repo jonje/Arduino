12e52963d794fb9a9c375a746c3b330cc312dcb2
Jonathan Jensen <jpjensen@utah.gov>
2022-07-06 14:49:17 -0600

    Fix directory structure and add some Serial outputs

--- sketch_moisture_sensor/sketch_moisture_sensor.ino
+++ sketch_moisture_sensor/sketch_moisture_sensor.ino
@@ -2,20 +2,29 @@ const int redLed = 2;
 const int yellowLed = 3;
 const int greenLed = 4;
 const int aZero = A0;
+int sensorValue;
 
 void setup() {
     pinMode(redLed, OUTPUT);
     pinMode(yellowLed, OUTPUT);
     pinMode(greenLed, OUTPUT);
+//    Serial.begin(9600);
+//    Serial.println("Reading from sensor....");
+    delay(2000);
     
 }
 
 void loop() {
     // Read the value from moisture sensor using analog pin
-    int sensorValue = analogRead(aZero);
+    sensorValue = analogRead(aZero);
+//    Serial.print("Initial Sensor value ");
+//    Serial.println(sensorValue);
     
     // Map the moisture sensors values to precentages
     // 550 is the value in dry soil
     sensorValue = map(sensorValue, 550, 0, 0, 100);
+//    Serial.print("Mapped value ");
+//    Serial.println(sensorValue);
+    delay(10000);
     
 }
