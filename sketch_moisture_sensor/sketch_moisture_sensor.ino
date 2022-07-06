3c32e161cbd8d293ba6a5df571501dcfbbb96d32
Jonathan Jensen <jpjensen@utah.gov>
2022-07-06 16:03:46 -0600

    Add digital light change function

--- sketch_moisture_sensor/sketch_moisture_sensor.ino
+++ sketch_moisture_sensor/sketch_moisture_sensor.ino
@@ -3,6 +3,7 @@ const int yellowLed = 3;
 const int greenLed = 4;
 const int aZero = A0;
 int sensorValue;
+String currentColor = "";
 
 void setup() {
     Serial.begin(9600);
@@ -30,13 +31,39 @@ void loop() {
     if (sensorValue >= 0 && sensorValue <= 33) {
       // Water is needed turn on red light
       Serial.println("RED");
+      changeLight("RED");
     } else if (sensorValue > 33 && sensorValue < 66) {
       // Water is in the mid range turn on yellow light
       Serial.println("YELLOW");
+      changeLight("YELLOW");
     } else {
       // Water is good turn on green light
       Serial.println("GREEN");
+      changeLight("GREEN");
     }
     delay(5000);
     
+}
+
+void changeLight(String color) {
+  if (color.equals(currentColor)) {
+    return;
+  }
+
+  if (color.equals("GREEN")) {
+    digitalWrite(redLed, LOW);
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    
+  } else if (color.equals("RED")) {
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(redLed, HIGH);
+  } else if (color.equals("YELLOW")) {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(yellowLed, HIGH);
+  }
+
+  currentColor = color;
 }
